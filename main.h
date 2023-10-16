d54fb615ab801d1e13b0aed5c98cc8f077b0f2ba
hagarSamy <hagarsami63@gmail.com>
2023-10-16 13:00:25 -0700

    Hola printf

--- main.h
+++ main.h
@@ -5,11 +5,18 @@
 int _printf(const char *format, ...);
 int _putchar(char c);
 
+/**
+ * struct prnt - a struct that deals with a specifier
+ * @ch: a charater
+ * @f: a function pointer
+ * p - typedef
+ */
+
 typedef struct prnt
 {
 	char ch;
 	void (*f)(va_list);
-}p;
+} p;
 
 int _printstr(char *str);
 int handle_format_specifier(char specifier, va_list list);
